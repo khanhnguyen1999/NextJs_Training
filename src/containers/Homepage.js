84acc0bd84ce0063d75021d170d500e3de079d44
khanhnguyen1999 <npkhanh17ck1@gmail.com>
2020-12-17 13:55:21 +0700

    completed part 1

---
+++ src/containers/Homepage.js
@@ -0,0 +1,3 @@
+export function Homepage (){
+    return <h1>homepage</h1>
+}
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,3 +1,23 @@
+import Link from 'next/link'
+
+const peoples = [
+    {v: "lambor",name:"khanh"},
+    {v: "ferarri",name:"test"},
+    {v: "bmw",name:"huy"}
+]
+
 export default function Details(){
-    return <h2>Details</h2>
+    return (
+        <div>
+            {
+                peoples.map(e=>(
+                    <div>
+                        <Link as={`/${e.v}/${e.name}`} href="/[vehicle]/[person]">
+                            <a>{`Navigate to ${e.name}'s ${e.v}`}</a>
+                        </Link>
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
